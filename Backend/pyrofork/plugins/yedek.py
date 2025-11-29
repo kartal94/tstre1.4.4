3762f04bc9c23747d14a653f42b3734f57e225d7
kartal788 <146070323+kartal788@users.noreply.github.com>
2025-11-29 15:58:51 +0300

    Update yedek.py

--- Backend/pyrofork/plugins/yedek.py
+++ Backend/pyrofork/plugins/yedek.py
@@ -55,9 +55,9 @@ async def database_status(_, message: Message):
         movies_count, series_count, storage_mb = stats
 
         text = (
-            f"Filmler: {movies_count:,}\n"
-            f"Diziler: {series_count:,}\n"
-            f"Depolama:{storage_mb} MB"
+            f"Filmler:             {movies_count:,}\n"
+            f"Diziler:            {series_count:,}\n"
+            f"Depolama:     {storage_mb} MB"
         )
 
         await message.reply_text(text)
