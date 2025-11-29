bfd03e1b2eb39ef8e2c74e33c2d2619c510ac53c
kartal788 <146070323+kartal788@users.noreply.github.com>
2025-11-29 13:58:37 +0300

    Update stats.py

--- Backend/pyrofork/plugins/stats.py
+++ Backend/pyrofork/plugins/stats.py
@@ -1,1 +1,15 @@
 
+from pyrogram import filters, Client, enums
+from pyrogram.types import Message
+from Backend.helper.custom_filter import CustomFilters
+
+@Client.on_message(filters.command('stats') & filters.private & CustomFilters.owner, group=10)
+async def stats_greeting(client: Client, message: Message):
+    """
+    /stats komutuna sadece selam mesajı gönderir
+    """
+    await message.reply_text(
+        "Selam! Stats komutu alındı 😊",
+        quote=True,
+        parse_mode=enums.ParseMode.HTML
+    )
